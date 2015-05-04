4d56a98893e38a1f8e031c5f2455da1446eb1797
fp0520141051 <fp0520141051@L1A1M11.fprestes.com.br>
2015-05-04 19:39:18 -0300

    deletado CadItensAleatorios

--- src/inicio/FrmCadItens.java
+++ src/inicio/FrmCadItens.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package inicio.CadItem;
+package inicio;
 
 import Ultilitarios.AutocompleteJComboBox;
 import Ultilitarios.StringSearchable;
@@ -15,12 +15,12 @@ import java.util.List;
  *
  * @author LucasKoury
  */
-public class FrmCadCatalogo extends javax.swing.JFrame {
+public class FrmCadItens extends javax.swing.JFrame {
     
     /**
      * Creates new form FrmCadCatalogo
      */
-    public FrmCadCatalogo() {
+    public FrmCadItens() {
         initComponents();
         int numOfMenus = cmbArea.getItemCount();
         
@@ -126,20 +126,20 @@ public class FrmCadCatalogo extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmCadCatalogo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCadItens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmCadCatalogo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCadItens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmCadCatalogo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCadItens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmCadCatalogo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCadItens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrmCadCatalogo().setVisible(true);
+                new FrmCadItens().setVisible(true);
             }
         });
     }
